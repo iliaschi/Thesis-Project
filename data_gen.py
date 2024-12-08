b65946a399c39535ccb03e5ca5f7fa59567abbc9
unknown <eliastsikoss@gmail.com>
2024-12-08 22:31:13 +0100

    step 1

--- data_gen.py
+++ data_gen.py
@@ -98,3 +98,79 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# pip install accelerate
+
+import torch
+from diffusers import FluxPipeline
+print('ok1')
+
+# pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+# pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+# prompt = "A cat holding a sign that says hello world"
+# image = pipe(
+#     prompt,
+#     guidance_scale=0.0,
+#     num_inference_steps=4,
+#     max_sequence_length=256,
+#     generator=torch.Generator("cpu").manual_seed(0)
+# ).images[0]
+# image.save("flux-schnell.png")
+
+
+# import torch
+# from diffusers import StableDiffusion3Pipeline
+
+# pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
+# pipe = pipe.to("cuda")
+
+# image = pipe(
+#     "A capybara holding a sign that reads Hello World",
+#     num_inference_steps=28,
+#     guidance_scale=3.5,
+# ).images[0]
+# image.save("capybara.png")
+
+
+from diffusers import BitsAndBytesConfig, SD3Transformer2DModel
+from diffusers import StableDiffusion3Pipeline
+import torch
+
+access_token = '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'
+
+model_id = "stabilityai/stable-diffusion-3.5-large-turbo"
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+model_nf4 = SD3Transformer2DModel.from_pretrained(
+    model_id,
+    subfolder="transformer",
+    quantization_config=nf4_config,
+    torch_dtype=torch.bfloat16,
+    token=access_token
+)
+
+t5_nf4 = T5EncoderModel.from_pretrained("diffusers/t5-nf4", torch_dtype=torch.bfloat16)
+
+pipeline = StableDiffusion3Pipeline.from_pretrained(
+    model_id, 
+    transformer=model_nf4,
+    text_encoder_3=t5_nf4,
+    torch_dtype=torch.bfloat16
+)
+pipeline.enable_model_cpu_offload()
+
+prompt = "A whimsical and creative image depicting a hybrid creature that is a mix of a waffle and a hippopotamus, basking in a river of melted butter amidst a breakfast-themed landscape. It features the distinctive, bulky body shape of a hippo. However, instead of the usual grey skin, the creature's body resembles a golden-brown, crispy waffle fresh off the griddle. The skin is textured with the familiar grid pattern of a waffle, each square filled with a glistening sheen of syrup. The environment combines the natural habitat of a hippo with elements of a breakfast table setting, a river of warm, melted butter, with oversized utensils or plates peeking out from the lush, pancake-like foliage in the background, a towering pepper mill standing in for a tree.  As the sun rises in this fantastical world, it casts a warm, buttery glow over the scene. The creature, content in its butter river, lets out a yawn. Nearby, a flock of birds take flight"
+
+image = pipeline(
+    prompt=prompt,
+    num_inference_steps=4,
+    guidance_scale=0.0,
+    max_sequence_length=512,
+).images[0]
+image.save("whimsical.png")
